68a83c4303d7745c78c8c40800f4a113e2bce4b3
Juan Antelo <juancantelo23@gmail.com>
2023-08-15 11:11:56 -0300

    pelis

---
+++ index.js
@@ -0,0 +1,52 @@
+const peliculasJSON = "peliculas.json";
+
+async function getJSONData(peliculas) {
+    return fetch(peliculas)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw Error(response.statusText);
+            }
+        })
+        .then(function(response) {
+            return { status: 'ok', data: response };
+        })
+        .catch(function(error) {
+            return { status: 'error', data: error };
+        });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    getJSONData(peliculasJSON).then(function(resultObj) {
+        if (resultObj.status === "ok") {
+            const peliculasArray = resultObj.data.peliculas;
+            showCategoriesList(peliculasArray);
+        }
+    });
+});
+
+function showCategoriesList(array) {
+    let htmlContentToAppend = "";
+    for (let i = 0; i < array.length; i++) {
+        htmlContentToAppend += `
+            <div class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="${array[i].image}" alt="peli image" class="img">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <div class="mb-1">
+                                <h4>${array[i].name}-${array[i].estreno} ${array[i].autores} </h4>
+                                <p>${array[i].genero}</p>
+                            </div>
+                        </div>
+                        <small class="text-muted"> </small>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+    document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+}
